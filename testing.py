6c1e3dc1be5cf3b9f57c7e33ad6ae650d1146fb3
joe <orbitalotter1@gmail.com>
2022-08-22 15:13:30 +1200

    Created first iteration, allowing the user to enter a simple ingredients query and get a list of recipe urls

--- testing.py
+++ testing.py
@@ -3,10 +3,11 @@ api_id="b2e4769a"
 api_key="ebb05e24a1f6b55f2f229af673c8ed2a"
 query=input("Enter a query:  ")
 request = requests.get("https://api.edamam.com/api/recipes/v2?type=public&q"
-                       "="+query+"&app_id="+api_id+"&app_key="+api_key)
+                       "="+query+"&app_id="+api_id+"&app_key="+api_key
+                       +"&dishType=Main%20course")
+
 request=request.json()
 links=[]
-print(type(request), type(request["hits"]))
 for item in request["hits"]:
     print(item["recipe"]["url"])
 print(links)
