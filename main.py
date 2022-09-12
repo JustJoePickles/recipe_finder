3ece8cd14c1a0e76121ead1934d1e6513a843563
joe <orbitalotter1@gmail.com>
2022-09-12 15:02:20 +1200

    Added filters and their labels along with entry box for ingredients. Trialled multiple options for filters (listbox, combobox) and settled on combobox. Additionally fixed a number of bugs that appeared when adding in the filters.

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from tkinter import *
 import requests
 from PIL import Image, ImageTk
-
+import math
+from tkinter import ttk
 
 class MainProgram():
     def __init__(self):
@@ -9,6 +10,8 @@ class MainProgram():
         self.root.title("Recipe Finder")
         self.root.geometry("800x500")
         self.root.configure(background="white")
+        self.font = ("Noto Sans SemiBold", 14)
+
         self.overlay = Overlay(self)
 
         self.homepage = HomePage(self)
@@ -42,7 +45,6 @@ class Overlay():
                             highlightthickness=0)
         self.window.grid(row=0, column=0, sticky="nsew", padx=40, pady=15)
 
-        self.font = ("Noto Sans SemiBold", 14)
 
         self.window.columnconfigure(0, weight=1)
         self.window.columnconfigure(1, weight=8)
@@ -62,22 +64,26 @@ class HomePage():
         self.reference = "homepage"
         self.blank = PhotoImage()
         self.grid_objects = []
+        self.filter_list=[]
         self.window = Frame(root.root, bg="blue", width=0, height=0)
         self.window.grid(row=0, column=0, sticky="nsew")
 
         self.window.columnconfigure(0, weight=1)
 
-        self.window.rowconfigure(0, weight=1)
-        self.window.rowconfigure(1, weight=2)
-        self.window.rowconfigure(2, weight=5)
+        self.window.rowconfigure(0, weight=2)
+        self.window.rowconfigure(1, weight=3)
+        self.window.rowconfigure(2, weight=1)
+        self.window.rowconfigure(3, weight=9)
 
         self.top_bar = Frame(self.window, bg="red")
         self.search = Frame(self.window, bg="green")
+        self.headers = Frame(self.window, bg="dark green")
         self.body = Frame(self.window, bg="yellow")
 
         self.top_bar.grid(row=0, column=0, sticky="nsew")
         self.search.grid(row=1, column=0, sticky="nsew")
-        self.body.grid(row=2, column=0, sticky="nsew")
+        self.headers.grid(row=2, column=0, sticky="nsew")
+        self.body.grid(row=3, column=0, sticky="nsew")
 
         self.top_bar.rowconfigure(0, weight=1)
         self.top_bar.columnconfigure(0, weight=1)
@@ -100,6 +106,54 @@ class HomePage():
 
         self.filters.grid(row=0, column=0, sticky="nsew")
         self.results.grid(row=0, column=1, sticky="nsew")
+
+        self.ingredients = Entry(self.search, font=root.font)
+        self.ingredients.pack(side=TOP, expand=True, fill='both', padx=20,
+                              pady=30)
+
+        self.filter_making()
+
+    def filter_making(self):
+        self.strings = [StringVar(), StringVar(), StringVar(),
+                   StringVar()]
+
+        # for s in strings:
+        #     s.set("Any")
+        # default.set("Any")
+
+        filter_list = ["Meal Type", "Dish Type", "Region of Origin",
+                          "Dietary Requirements"]
+        filter_options =[["Any", "Breakfast", "Lunch/Dinner", "Snack"],
+                         ["Any", "Maincourse", "Side Dish", "Starter",
+                          "Desserts",
+                          "Alcohol Cocktail"], ["Any", "Italian", "Chinese",
+                                                "Indian", "Japanese",
+                                                "korean", "Mediterranean",
+                                                "Mexican", "Central Europe",
+                                                "Eastern Europe", "Asian",
+                                                "British", "American",
+                                                "South American"], ["Any",
+                         "Vegetarian", "Vegan", "Dairy-Free", "Peanut-Free"]]
+        for i in range(8):
+            print(i, math.floor(i/2))
+            # self.filters.rowconfigure(0, weight=1)
+            if i % 2 == 0:
+                label = Label(self.filters, bg="green", anchor = "w",
+                              text = filter_list[math.floor(i/2)])
+                label.pack(side=TOP, expand=True, fill='both', padx=(0,20),
+                              pady=5)
+            if i % 2 == 1:
+                menu = ttk.Combobox(self.filters, textvariable=self.strings[
+                    math.floor(i/2)])
+                menu['values'] = filter_options[
+                    math.floor(i/2)]
+                menu.current(0)
+                menu['state'] = 'readonly'
+                self.filter_list.append(menu)
+                menu.pack(side=TOP, expand=True, fill='both', padx=(20,0),
+                              pady=5)
+
+        print(self.filter_list)
 
 if __name__ == "__main__":
     MainProgram()
